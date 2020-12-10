ab294e62e9773457ff160e8aee89bb7fc74b0ca3
Baul Hoa <baul.hoa@v-key.com>
2020-12-10 21:24:39 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='dlp',
     packages=['dlp'],
-    version='1.0.34',
+    version='1.0.35',
     license='MIT', # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Code generator',
     author='BAO',
